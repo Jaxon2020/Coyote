a77c491dbd94fa8cbc319b17a361c7d9989c29e2
Jaxon2020 <61923016+Jaxon2020@users.noreply.github.com>
2022-06-27 12:55:23 +0000

    Update Penetration.py

--- Penetration.py
+++ Penetration.py
@@ -1,14 +1,15 @@
-from asyncio.windows_events import NULL
-from operator import index
-from pydoc import visiblename
+
+
+
 import subprocess
-from tkinter.tix import COLUMN
 import PySimpleGUI as sg
 import nmap
 import pandas as ps
 import os
 import GUItraffic as Packets
 import shlex
+import sqlite3 as sql
+
 
 ninfo = open('nmapinfo.txt', 'r')
 contents = ninfo.read()
@@ -64,7 +65,7 @@ def NMAP():
                 print("\n"'State : %s' % nm[values['-IP-']].state())
                 
                 
-                print( "\n", test_data[['protocol', 'port', 'name', 'state', 'reason']])
+                print( "\n", test_data[['protocol', 'port', 'name','version', 'state', 'reason' ]])
             except:
                 print("Error!")
             
@@ -125,7 +126,7 @@ def PenTest():
         
         [sg.pin(sg.Text("Waiting for Data", key='-DC-', visible=False))],
         
-
+        
 
     ]
 
@@ -137,9 +138,12 @@ def PenTest():
 
             [sg.Text("Coyote")],
             [sg.Column(leftcol, element_justification='c') ,sg.Text(ART, key='-ART-'), sg.pin(sg.Text("Waiting for Data", key='-WEB-', visible=False))],
-            [sg.Text("Enter Commands"),sg.Input(key='-IN-'),sg.Button("Intiate CLI", key='-CLI-')],
+            [sg.Text("", key='-EXINFO-', visible=False)],
+            [sg.Text("Add Info:"),sg.Input(key='-INFOIN-'),sg.Button("Add Info", key='-ADEXINFO-'), sg.Button("NMAP", key="NMAP")],
+            [sg.Text("CLI:"),sg.Input(key='-IN-'),sg.Button("Intiate CLI", key='-CLI-')],
             [sg.Output(size=(40,15), key='-1OUT-')],
             [sg.Button("Display", key="-ESEND-", bind_return_key=True)],
+            [sg.Button("Add to DB", key="-DBSEND-")],
             [sg.Button("Exit", button_color=('white', 'firebrick3'), key='Exit') ]
             
             
@@ -150,11 +154,11 @@ def PenTest():
         event, values = window.read()
         if event == "-ESEND-":
             
-            webr = NULL
+            webr = 0
             nmr = ps.read_csv('nmapdump.csv', sep = ';', header = 0)
             if nmr.empty:
                 print("No data to display")
-            elif(webr == NULL):
+            elif(webr == 0):
                 window['-ART-'].update(ART2)
                 window['-DC-'].update((nmr['protocol'].astype(str) + " " + nmr['port'].astype(str) + " " + nmr['name'].astype(str)).to_string(index=False), visible=True)
                 #window['-WEB-'].update((nmr['port'].astype(str) + " " + nmr['name'].astype(str)).to_string(index=False), visible=True)
@@ -163,7 +167,8 @@ def PenTest():
                   window['-ART-'].update(ART3)
                   window['-DC-'].update((nmr['protocol'].astype(str) + " " + nmr['port'].astype(str) + " " + nmr['name'].astype(str)).to_string(index=False), visible=True)
                   window['-INFOTITLE-'].update(("Host:" + " " + nmr['host'][0] + "(" + nmr['hostname'][0] + ")"),visible=True)
-                
+        if event == '-ADEXINFO-':
+            window['-EXINFO-'].update(values['-INFOIN-'], visible=True)
         if event == "-CLI-":
             try: 
                 args = shlex.split(values['-IN-'])
@@ -172,6 +177,35 @@ def PenTest():
                 print(output.decode('utf-8'))
             except:
                 print("Error!")
+        if event == "-DBSEND-":
+
+            con = sql.connect("Coyote.db")
+
+            cursor = con.cursor()
+       
+            if os.path.exists("Coyote.db"):
+                print("Using Coyote.db")
+            else:
+                print("The file does not exist")
+                cursor.execute("CREATE TABLE nmap (host TEXT, protocol TEXT, port INT, name TEXT, state TEXT, product TEXT, extrainfo TEXT, reason TEXT, version TEXT)")
+
+
+            nmr = ps.read_csv('nmapdump.csv', sep = ';', header = 0)
+            nmaprows = []
+            for i in nmr.index:
+                nmaprows = [nmr['host'][i], nmr['protocol'][i], (nmr['port'][i]), nmr['name'][i],nmr['state'][i],nmr['product'][i],nmr['extrainfo'][i],nmr['reason'][i],nmr['version'][i]]
+                sql_command = 'INSERT INTO nmap VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
+                cursor.execute(sql_command, nmaprows)
+            
+           
+            con.commit()
+            rows = cursor.execute("SELECT host, protocol, port, name, state, product, extrainfo, reason, version FROM nmap").fetchall()
+
+            print(rows)
+
+        if event == 'NMAP':
+            NMAP()
+
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
@@ -210,7 +244,6 @@ def Gobuster():
             break
 
     window.close()
-
 
 
 
