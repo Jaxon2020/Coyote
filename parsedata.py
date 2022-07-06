ffa19ddf0496e515b683e2e98a6f374f0734fefb
Jaxon2020 <jaxonallenreeves@gmail.com>
2022-07-05 23:15:41 -0400

    ad

---
+++ parsedata.py
@@ -0,0 +1,18 @@
+import re   
+
+
+def parsexdata():
+    with open('exploitdata.txt','r') as file:
+        countriesStr = file.read()
+    #print(countriesStr)
+
+
+    t = countriesStr.replace("-", "")
+
+    res = re.sub(' +', ' ', t)
+
+  
+    newres = re.sub(r'[^\x00-\x7f]', '', res)
+    return newres
+
+#print(parsexdata())
